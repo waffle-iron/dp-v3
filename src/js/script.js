0b123c0669f7bfc15fe71b9114e7a58e9758bf9d
Andrew Fischer <afischer15@mac.com>
2015-12-27 17:17:56 -0500

    animated section menu, ripped out jquery

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+
+// DP Javascript
+// Copyright Andrew Fischer 2015
+
+////////////////
+//// HEADER ////
+////////////////
+
+var secBtn = document.getElementById('section-dropdown-btn');
+var dropdown = document.getElementById('section-dropdown');
+secBtn.addEventListener("click", function(){
+    'use strict';
+    dropdown.classList.toggle('closed');
+    secBtn.classList.toggle('closed');
+});
